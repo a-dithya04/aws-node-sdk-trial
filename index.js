24d574a8e8b639fa3291fa71b6b742537633d20e
a-dithya04 <senthiladithya04@gmail.com>
2024-02-25 23:06:26 +0530

    Update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// This is used for getting user input.
+
 import { createInterface } from "readline/promises";
 
 import {
@@ -12,19 +12,16 @@ import {
 } from "@aws-sdk/client-s3";
 
 export async function main() {
-    // A region and credentials can be declared explicitly. For example
-    // `new S3Client({ region: 'us-east-1', credentials: {...} })` would
-    //initialize the client with those settings. However, the SDK will
-    // use your local configuration and credentials if those properties
-    // are not defined here.
+
     const s3Client = new S3Client({
-        region: "ap-southeast-1", credentials:{ 
-            accessKeyId: , 
-            secretAccessKey:  }});
+        region: "ap-southeast-1", credentials: {
+            accessKeyId: "AKIA3FLD5BGUYKSJMLO6",
+            secretAccessKey: "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"
+        }
+    });
 
 
-    // Create an Amazon S3 bucket. The epoch timestamp is appended
-    // to the name to make it unique.
+
     const bucketName = `test-bucket-${Date.now()}`;
     await s3Client.send(
         new CreateBucketCommand({
@@ -79,13 +76,11 @@ export async function main() {
         }
 
         // Once all the objects are gone, the bucket can be deleted.
+        //In AWS S3, only empty buckets can be deleted.
         await s3Client.send(new DeleteBucketCommand({ Bucket: bucketName }));
     }
 }
 
-// Call a function if this file was run directly. This allows the file
-// to be runnable without running on import.
-import { fileURLToPath } from "url";
-if (process.argv[1] === fileURLToPath(import.meta.url)) {
-    main();
-}
+
+main();
+
